e844f6f8d866017c2101a1cd40bdca8a2f13f93e
Stanislav Novikov <stanislav.a.novikov@gmail.com>
2020-03-26 22:15:02 +0300

    TOP-3 improve scheme after review

--- scripts/sql/TOP-3.sql
+++ scripts/sql/TOP-3.sql
@@ -1,42 +1,41 @@
-CREATE TABLE account (
+CREATE TABLE IF NOT EXISTS account (
     account_id bigint PRIMARY KEY,
     login varchar(255) NOT NULL,
     email varchar(255),
     name varchar(255)
 );
 
-CREATE TABLE repository (
+CREATE TABLE IF NOT EXISTS repository (
     repo_id bigint PRIMARY KEY,
     path varchar(255) NOT NULL
 );
 
-CREATE TABLE pull_request (
+CREATE TABLE IF NOT EXISTS pull_request (
     pull_request_id bigint PRIMARY KEY,
     author_id bigint NOT NULL,
-    creation_date date NOT NULL,
-    last_update_date date NOT NULL,
+    creation_time timestamp NOT NULL,
+    last_update_time timestamp NOT NULL,
     status int NOT NULL,
     repo_id bigint NOT NULL,
     FOREIGN KEY (author_id) REFERENCES account(account_id),
     FOREIGN KEY (repo_id) REFERENCES repository(repo_id)
 );
 
-CREATE TABLE approve (
+CREATE TABLE IF NOT EXISTS approve (
     approve_id bigint PRIMARY KEY,
     author_id bigint NOT NULL,
     pull_request_id bigint NOT NULL,
     status int NOT NULL,
-    date date NOT NULL,
+    time timestamp NOT NULL,
     FOREIGN KEY (author_id) REFERENCES account(account_id),
     FOREIGN KEY (pull_request_id) REFERENCES pull_request(pull_request_id)
 );
 
-CREATE TABLE commit (
-    commit_id bigint PRIMARY KEY,
-    sha bigint NOT NULL,
+CREATE TABLE IF NOT EXISTS commit (
+    sha char(40) PRIMARY KEY,
     author_id bigint NOT NULL,
     pull_request_id bigint NOT NULL,
-    creation_date date NOT NULL,
+    creation_time timestamp NOT NULL,
     added_lines int NOT NULL,
     deleted_lines int NOT NULL,
     repo_id bigint NOT NULL,
@@ -45,11 +44,11 @@ CREATE TABLE commit (
     FOREIGN KEY (repo_id) REFERENCES repository(repo_id)
 );
 
-CREATE TABLE comment (
+CREATE TABLE IF NOT EXISTS comment (
     comment_id bigint PRIMARY KEY,
-    creation_date date NOT NULL,
+    creation_time timestamp NOT NULL,
     author_id bigint NOT NULL,
-    commit_id bigint NOT NULL,
+    commit_hash char(40) NOT NULL,
     FOREIGN KEY (author_id) REFERENCES account(account_id),
-    FOREIGN KEY (commit_id) REFERENCES commit(commit_id)
+    FOREIGN KEY (commit_hash) REFERENCES commit(sha)
 );
